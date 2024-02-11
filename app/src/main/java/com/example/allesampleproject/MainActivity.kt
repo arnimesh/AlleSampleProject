063f72bcf21fbdd58c92d819291175f56faf8d7d
Arnimesh <arnimesh08@gmail.com>
2024-02-11 23:46:53 +0530

    linked listeners to activity

--- app/src/main/java/com/example/allesampleproject/MainActivity.kt
+++ app/src/main/java/com/example/allesampleproject/MainActivity.kt
@@ -1,46 +1,118 @@
 package com.example.allesampleproject
 
 import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
 import android.content.res.Resources
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SnapHelper
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.alle2.OnSnapPositionChangeListener
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnSnapPositionChangeListener {
+
     private lateinit var recyclerView: RecyclerView
-    private lateinit var imageView: ImageView
     private lateinit var imageList: ArrayList<ImagePics>
-    private lateinit var imagePicsAdapter: ImagePicsAdapter
+    private lateinit var ImagePicsAdapter: ImagePicsAdapter
+    private lateinit var imageView: ImageView
+    private lateinit var btn: Button
+    private val GALLERY_CODE = 1221
+    private var currImagePositon = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         init()
+        btn = findViewById(R.id.button)
+        btn.setOnClickListener(View.OnClickListener {
+            openGalleryForImages()
+        })
+
+
     }
 
-    private fun init() {
-        addDataToList()
+    private fun openGalleryForImages() {
 
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "image/*"
+        startActivityForResult(intent, GALLERY_CODE);
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK && requestCode == GALLERY_CODE) {
+            if (data?.clipData != null) {
+                val count = data.clipData?.itemCount
+                Log.d("test", data.data.toString())
+                for (i in 0..<count!!) {
+                    val uri: Uri = data.clipData?.getItemAt(i)!!.uri
+                    imageList.add(currImagePositon, ImagePics(uri))
+                    ImagePicsAdapter.notifyItemInserted(currImagePositon)
+                    imageView.setImageURI(uri)
+                   // loadImage(this, imageView, uri)
+
+                }
+            } else if (data?.data != null) {
+                val uri = data.data
+                imageList.add(currImagePositon, ImagePics(uri))
+                ImagePicsAdapter.notifyItemInserted(currImagePositon)
+                imageView.setImageURI(uri)
+
+                //loadImage(this, imageView, uri)
+
+            }
+        }
+    }
+
+
+    private fun init() {
         recyclerView = findViewById(R.id.recylerView)
         recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        recyclerView.layoutManager = ScaleLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        imageList = ArrayList()
+        val snapHelper: SnapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(recyclerView)
+        addDataToList()
 
 
-        imagePicsAdapter = ImagePicsAdapter(imageList, this)
-        recyclerView.adapter = imagePicsAdapter
+        recyclerView.attachSnapHelperWithListener(
+            snapHelper,
+            SnapOnScrollListener.Behavior.NOTIFY_ON_SCROLL,
+            this
+        )
 
+        ImagePicsAdapter = ImagePicsAdapter(imageList, this)
+        recyclerView.adapter = ImagePicsAdapter
+        ImagePicsAdapter.setOnItemClickListener(object : ImagePicsAdapter.onItemClickListener {
 
+            override fun onItemClick(pos: Int) {
+                val position = pos % imageList.size
+//                Log.d("test123", imageList[position].foodImage.toString())
+//                Log.d("test", position.toString())
+                imageView = findViewById(R.id.imageView1)
+                imageView.setImageURI(imageList[position].imagePics)
+            }
+        })
     }
 
 
     private fun addDataToList() {
-        for (i in 1..100){
-            imageList
+        for (i in 1..50) {
             imageList.add(ImagePics(parseDrawableImage(R.drawable.one)))
             imageList.add(ImagePics(parseDrawableImage(R.drawable.two)))
             imageList.add(ImagePics(parseDrawableImage(R.drawable.three)))
@@ -63,4 +135,24 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    override fun onSnapPositionChange(pos: Int) {
+        currImagePositon = pos
+        imageView = findViewById(R.id.imageView1)
+        imageView.setImageURI(imageList[pos].imagePics)
+
+       // loadImage(imageView.context, imageView, imageList[pos].imagePics)
+    }
+
+//    private fun loadImage(context: Context, image: ImageView, uri: Uri?) {
+//        Glide.with(this)
+//            .load(uri)
+//            .apply(
+//                RequestOptions()
+//                    .placeholder(R.drawable.test) // Placeholder image
+//                    .error(R.drawable.test) // Error image in case of loading failure
+//            )
+//            .into(imageView)
+//    }
 }
+
+
